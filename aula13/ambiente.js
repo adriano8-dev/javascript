226ad08ccc35a9e5e2551fe6199a7b5ace23d8e0
Adriano Melo <meloadriano766@gmail.com>
2026-05-22 19:22:33 -0300

    estruturas de repetições com teste no inicio e no final

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+// Repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+// Repetição com teste lógico no inicio
+/*
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
